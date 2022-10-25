282696804679d560a3f6ce39ee6541a723008eac
Kaique Vinicius <cyberkaidev@gmail.com>
2022-10-25 09:00:23 -0300

    :tada: initial commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Kaique Vinicius Cirqueira Dourado on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pokedex/Views/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Kaique Vinicius Cirqueira Dourado on 24/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var pokemon = [PokemonEntry]()
+    @State var searchText = ""
+    
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(searchText == "" ? pokemon: pokemon.filter( {$0.name.contains(searchText.lowercased())} )) {
+                    entry in
+                    
+                    HStack {
+                        PokemonImage(imageLink: "\(entry.url)")
+                            .padding(.trailing, 20)
+                        
+                        NavigationLink("\(entry.name)".capitalized ,destination: Text(entry.name))
+                    }
+                }
+            }
+            
+            .onAppear {
+                PokeApi().getData() { pokemon in
+                    self.pokemon = pokemon
+                }
+            }
+            .searchable(text: $searchText)
+            .navigationTitle("Pokedex")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
